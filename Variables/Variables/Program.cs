514b7afd1ee3a11f611a19e460436db760efbc46
MrSquid <47063311+MrSquid25@users.noreply.github.com>
2023-05-03 18:08:42 +0200

    Add files via upload

---
+++ Variables/Variables/Program.cs
@@ -0,0 +1,12 @@
+﻿// implicito
+
+int numero = 10;
+
+//explicito
+
+var numero2 = 12;
+
+System.Console.WriteLine(numero.GetType());
+System.Console.WriteLine(numero2.GetType());
+
+System.Console.WriteLine(numero + numero2);
